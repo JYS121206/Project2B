b52a7c1904b9ffcf02453c37920dc27108a85519
DESKTOP-LB5KTN8\ssal8 <ssal8888@naver.com>
2023-01-26 22:22:38 +0900

    TestScene 수집UI 부분 터치로 변환

---
+++ Assets/Scripts/ect/GetRabbitMode.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class GetRabbitMode : MonoBehaviour
+{
+    public GameObject UIPopRUGet;
+    public GameObject UIPopGetRB;
+
+    public GameObject cube;
+
+    public Button btnGet;
+    public Button btnPass;
+    public Button btnCloseRUGet;
+
+    public Button btnConfirm;
+    public Button btnContinue;
+    public Button btnCloseGetRB;
+
+    public List<GameObject> rabbitList = new List<GameObject>();
+    GameObject curRabbit;
+    GameObject target;
+
+    public ARRaycastManager arRaycaster;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        GetRabbit();
+    }
+
+    public void GetRabbit()
+    {
+        if (rabbitList == null)
+            return;
+
+        if (Input.touchCount > 0)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+
+            //UI뒤 오브젝트 클릭 못하게 하기
+            if (EventSystem.current.IsPointerOverGameObject())
+                return;
+
+            else
+            {
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Debug.Log(hit.transform.gameObject);
+                    target = hit.transform.gameObject;
+                    UIPopRUGet.SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void CloseRUGet()
+    {
+        UIPopRUGet.SetActive(false);
+
+        Destroy(target, 1f);
+    }
+
+    public void OpenGetRB()
+    {
+        UIPopRUGet.SetActive(false);
+        UIPopGetRB.SetActive(true);
+    }
+
+    public void CloseGetRB()
+    {
+        UIPopGetRB.SetActive(false);
+        Destroy(target);
+    }
+
+    public void SpawnRabbit()
+    {
+        int rabbitIdx = Random.Range(0, rabbitList.Count);
+
+        //Instantiate 생성객체 ,            위치값 ,               회전값
+        curRabbit = Instantiate(rabbitList[rabbitIdx], new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+    }
+}
