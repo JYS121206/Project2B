d3be48188de2bb67f5f7547b57292cb3cda3dc92
JYS121206 <p.uncia1775@gmail.com>
2023-01-27 15:06:26 +0900

    유동적인 해상도에 맞춰 오브젝트 사이즈 조정

---
+++ Assets/Scripts/ect/SetSize.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetSize : MonoBehaviour
+{
+    float setSize;
+
+    void Start()
+    {
+        float height = Camera.main.orthographicSize * 2;
+        float width = height * Screen.width / Screen.height;
+
+        Debug.Log($"height: {height} | width: {width}");
+
+        setSize = (width / 4.5f) * 1;
+
+        if (width == 4.5f)
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+
+        gameObject.transform.localScale = new Vector3(setSize, setSize, setSize);
+        gameObject.transform.position = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+    }
+}
